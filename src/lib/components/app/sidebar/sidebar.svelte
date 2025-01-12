9c357732e3f24fce1c8ef1518d0055212e12e873
paul allan palacio <palaciopaulbermudez@gmail.com>
2025-01-12 13:32:52 +0800

    fix sidebar component

---
+++ src/lib/components/app/sidebar/sidebar.svelte
@@ -0,0 +1,134 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { AlignJustifyIcon } from 'lucide-svelte';
+	import { page } from '$app/stores';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+
+	let sidebarOpen = $state(false);
+
+	function toggleSidebar() {
+		sidebarOpen = !sidebarOpen;
+	}
+
+	function isActive(href: string): boolean {
+		return $page.url.pathname === href;
+	}
+</script>
+
+<aside
+	class={cn(
+		' fixed left-0 top-0 z-50 flex min-h-screen w-48 -translate-x-full flex-col border-r bg-white p-4 transition-transform md:translate-x-0 md:bg-transparent',
+		sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+	)}
+>
+	<div class="flex flex-col space-y-5">
+		<div class="flex-1 overflow-y-auto">
+			<h1 class="py-2 text-sm text-slate-400">About Me</h1>
+			<ul class=" text-sm">
+				<li>
+					<a
+						href="/about-me"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Introduction</span>
+					</a>
+				</li>
+				<li>
+					<a
+						href="/about-me"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>CV</span>
+					</a>
+				</li>
+				<li>
+					<a
+						href="/about-me"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Contact</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<Separator class="my-4" />
+		<div class="flex-1 overflow-y-auto">
+			<h1 class="py-2 text-sm text-slate-400">Web Development</h1>
+			<ul class=" text-sm">
+				<li>
+					<a
+						href="/web-dev/coursequest"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Full Stack</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<Separator class="my-4" />
+		<div class="flex-1 overflow-y-auto">
+			<h1 class="py-2 text-sm text-slate-400">Web Designs</h1>
+			<ul class=" text-sm">
+				<li>
+					<a
+						href="/web-design/figma/readi"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Figma</span>
+					</a>
+				</li>
+				<li>
+					<a
+						href="/web-design/framer"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Framer</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<Separator class="my-4" />
+		<div class="flex-1 overflow-y-auto">
+			<h1 class="py-2 text-sm text-slate-400">Socials</h1>
+			<ul class=" text-sm">
+				<li>
+					<a
+						href="/socials"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Dribble</span>
+					</a>
+				</li>
+				<li>
+					<a
+						href="/socials"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Github</span>
+					</a>
+				</li>
+				<li>
+					<a
+						href="/socials"
+						class="flex items-center space-x-2 rounded p-1 font-medium transition-all duration-200 ease-in-out hover:bg-secondary"
+					>
+						<span>Linkedin</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</aside>
+
+<header class="flex h-20 w-full border-b bg-white md:hidden">
+	<div class="container flex h-full items-center justify-between">
+		<a href="/">
+			<h1 class="text-3xl font-bold">Portfolio</h1>
+		</a>
+
+		<Button size="icon" variant="ghost" onclick={toggleSidebar}>
+			<AlignJustifyIcon />
+		</Button>
+	</div>
+</header>
--- src/routes/(app)/about-me/+page.svelte
+++ src/routes/(app)/about-me/+page.svelte
@@ -11,248 +11,159 @@
 	}
 </script>
 
-<main class="space-24 container">
+<main class=" container">
 	<div class="-mt-4 space-y-4">
 		<div>
 			<h1 class="text-slate-400">About Me</h1>
 		</div>
-		{#if section === 'about'}
-			<div>
-				<div class="text-xl font-bold text-primary md:text-3xl">
-					Hi, I'm Paul Allan Palacio, a passionate Web Developer and UI/UX Designer. My journey
-					began with C# and VB.NET, but over time, I discovered a deep love for crafting beautiful,
-					user-centered interfaces and websites. I thrive on blending creativity with functionality
-					to create engaging digital experiences.
+
+		<div>
+			<div class="text-xl font-bold text-primary md:text-3xl">
+				Hi, I'm Paul Allan Palacio, a passionate Web Developer and UI/UX Designer. My journey began
+				with C# and VB.NET, but over time, I discovered a deep love for crafting beautiful,
+				user-centered interfaces and websites. I thrive on blending creativity with functionality to
+				create engaging digital experiences.
+			</div>
+		</div>
+		<div>
+			<Separator class=" my-12 " />
+		</div>
+		<div class="grid grid-cols-1 gap-6 py-4 md:grid-cols-7">
+			<div
+				class="col-span-2 row-span-4 rounded-lg border bg-white shadow-md transition hover:scale-105"
+			>
+				<div
+					class="flex items-center justify-center gap-4 px-0 py-12 font-bold text-slate-400 transition-all duration-300 ease-in-out hover:scale-105 hover:text-primary hover:underline"
+				>
+					<a
+						href="https://drive.google.com/file/d/1Brjs4gfIb75Aq7qjVFu0hqq9vsH-cg-q/view?usp=drive_link"
+						target="_blank"
+					>
+						View CV/Resume
+					</a>
+					<FileUserIcon />
 				</div>
 			</div>
+			<div
+				class="col-span-2 row-span-4 rounded-lg border bg-white shadow-md transition hover:scale-105"
+			>
+				<div
+					class="flex items-center justify-center gap-4 px-0 py-12 font-bold text-slate-400 transition-all duration-300 ease-in-out hover:scale-105 hover:text-primary hover:underline"
+				>
+					<a href="mailto:palaciopaulbermudez@gmail.com" target="_blank"> Send a Message </a>
+					<MessageCircleCode />
+				</div>
+			</div>
+		</div>
+
+		<div class=" pt-24">
+			<div class="text-xl font-bold text-primary md:text-3xl">Tech Stack</div>
 			<div>
 				<Separator class=" my-12 " />
 			</div>
-			<div class="grid grid-cols-1 gap-6 py-4 md:grid-cols-7">
-				<div
-					class="col-span-2 row-span-4 rounded-lg border bg-white shadow-md transition hover:scale-105"
+			<div class="grid grid-cols-2 gap-4 rounded-lg p-4 md:grid-cols-4">
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-orange-500 hover:text-orange-500"
+					variant="outline"
 				>
-					<div
-						class="flex items-center justify-center gap-4 px-0 py-12 font-bold text-slate-400 transition-all duration-300 ease-in-out hover:scale-105 hover:text-primary hover:underline"
-					>
-						<a
-							href="https://drive.google.com/file/d/1Brjs4gfIb75Aq7qjVFu0hqq9vsH-cg-q/view?usp=drive_link"
-							target="_blank"
-						>
-							View CV/Resume
-						</a>
-						<FileUserIcon />
-					</div>
-				</div>
-				<div
-					class="col-span-2 row-span-4 rounded-lg border bg-white shadow-md transition hover:scale-105"
+					<Icon icon="flowbite:html-solid" class=" h-4 w-4" />HTML
+				</Badge>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-500 hover:text-blue-500"
+					variant="outline"
 				>
-					<div
-						class="flex items-center justify-center gap-4 px-0 py-12 font-bold text-slate-400 transition-all duration-300 ease-in-out hover:scale-105 hover:text-primary hover:underline"
-					>
-						<a href="mailto:palaciopaulbermudez@gmail.com" target="_blank"> Send a Message </a>
-						<MessageCircleCode />
-					</div>
-				</div>
-				<button
-					class="col-span-3 row-span-4 transition hover:scale-105 focus:outline-none"
-					onclick={() => toggleSection('production')}
+					<Icon icon="flowbite:css-solid" class="h-4 w-4" />CSS
+				</Badge>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-red-600 hover:text-red-600"
+					variant="outline"
 				>
-					<div
-						class="flex cursor-pointer items-center justify-center gap-4 px-16 py-12 font-bold text-slate-400 hover:text-primary hover:underline md:px-24"
-					>
-						Tools I use for building a website
-						<MoveRight />
-					</div>
-				</button>
+					<Icon icon="ri:svelte-fill" class="h-4 w-4" />Svelte
+				</Badge>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-red-600 hover:text-red-600"
+					variant="outline"
+				>
+					<Icon icon="ri:svelte-fill" class="h-4 w-4" />Sveltekit
+				</Badge>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-sky-400 hover:text-sky-400"
+					variant="outline"
+				>
+					<Icon icon="mdi:tailwind" class="h-4 w-4" />Tailwind CSS
+				</Badge>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-sky-700 hover:text-sky-700"
+					variant="outline"
+				>
+					<Icon icon="tabler:brand-typescript" class="h-4 w-4" />Typescript
+				</Badge>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-sky-700 hover:text-sky-700"
+					variant="outline"
+				>
+					<Icon icon="tabler:brand-typescript" class="h-4 w-4" />Javascript
+				</Badge>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-purple-500 hover:text-purple-500"
+					variant="outline"><Icon icon="nonicons:c-sharp-16" class="h-4 w-4" />C#</Badge
+				>
+
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
+					variant="outline"><Icon icon="simple-icons:shadcnui" class="h-4 w-4" />Shadcn</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-green-500 hover:text-green-500"
+					variant="outline"><Icon icon="ri:supabase-line" class="h-4 w-4" />Supabase</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-lime-500 hover:text-lime-500"
+					variant="outline"><Icon icon="simple-icons:drizzle" class="h-4 w-4" />Drizzle ORM</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-red-600 hover:text-red-600"
+					variant="outline"><Icon icon="mdi:git" class="h-4 w-4" />Git</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
+					variant="outline"><Icon icon="mdi:github" class="h-4 w-4" />Github</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
+					variant="outline"><Icon icon="gg:vercel" class="h-4 w-4" />Vercel</Badge
+				>
+				<!-- <Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
+					variant="outline"><Icon icon="gg:vercel" class="h-4 w-4" />z.com</Badge
+				> -->
 			</div>
-		{:else if section === 'production'}
-			<div class="slide-in transform transition-transform">
-				<div class="text-xl font-bold text-primary md:text-3xl">
-					Here are the frameworks, libraries & tools I use for building a website:
-				</div>
-				<div>
-					<Separator class=" my-12 " />
-				</div>
+		</div>
 
-				<div class="grid grid-cols-1 gap-6 py-4 md:grid-cols-7">
-					<div class="col-span-4 row-span-4 grid grid-cols-2 gap-4 rounded-lg p-4 md:grid-cols-3">
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-orange-500 hover:text-orange-500"
-							variant="outline"
-						>
-							<Icon icon="flowbite:html-solid" class=" h-4 w-4" />HTML
-						</Badge>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-500 hover:text-blue-500"
-							variant="outline"
-						>
-							<Icon icon="flowbite:css-solid" class="h-4 w-4" />CSS
-						</Badge>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-red-600 hover:text-red-600"
-							variant="outline"
-						>
-							<Icon icon="ri:svelte-fill" class="h-4 w-4" />Svelte
-						</Badge>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-red-600 hover:text-red-600"
-							variant="outline"
-						>
-							<Icon icon="ri:svelte-fill" class="h-4 w-4" />Sveltekit
-						</Badge>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-sky-400 hover:text-sky-400"
-							variant="outline"
-						>
-							<Icon icon="mdi:tailwind" class="h-4 w-4" />Tailwind CSS
-						</Badge>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-sky-700 hover:text-sky-700"
-							variant="outline"
-						>
-							<Icon icon="tabler:brand-typescript" class="h-4 w-4" />Typescript
-						</Badge>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-sky-700 hover:text-sky-700"
-							variant="outline"
-						>
-							<Icon icon="tabler:brand-typescript" class="h-4 w-4" />Javascript
-						</Badge>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-purple-500 hover:text-purple-500"
-							variant="outline"><Icon icon="nonicons:c-sharp-16" class="h-4 w-4" />C#</Badge
-						>
-
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
-							variant="outline"><Icon icon="simple-icons:shadcnui" class="h-4 w-4" />Shadcn</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-green-500 hover:text-green-500"
-							variant="outline"><Icon icon="ri:supabase-line" class="h-4 w-4" />Supabase</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-lime-500 hover:text-lime-500"
-							variant="outline"
-							><Icon icon="simple-icons:drizzle" class="h-4 w-4" />Drizzle ORM</Badge
-						>
-					</div>
-
-					<button
-						class="col-span-3 row-span-4 transition hover:scale-105 focus:outline-none"
-						onclick={() => toggleSection('deployment')}
-					>
-						<div
-							class="flex cursor-pointer items-center justify-center gap-4 px-12 py-12 font-bold text-slate-400 hover:text-primary hover:underline md:px-24"
-						>
-							Collaboration & deployment
-							<MoveRight />
-						</div>
-					</button>
-				</div>
+		<div class=" pt-24">
+			<div class="text-xl font-bold text-primary md:text-3xl">Design Tools</div>
+			<div>
+				<Separator class=" my-12 " />
 			</div>
-		{:else if section === 'deployment'}
-			<div class="slide-in transform transition-transform">
-				<div class="text-xl font-bold text-primary md:text-3xl">
-					Here are the tools I use for team collboration & deployment:
-				</div>
-				<div>
-					<Separator class=" my-12 " />
-				</div>
 
-				<div class="grid grid-cols-1 gap-6 py-4 md:grid-cols-7">
-					<div class="col-span-4 row-span-4 grid grid-cols-2 gap-4 rounded-lg p-4 md:grid-cols-3">
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-red-600 hover:text-red-600"
-							variant="outline"><Icon icon="mdi:git" class="h-4 w-4" />Git</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
-							variant="outline"><Icon icon="mdi:github" class="h-4 w-4" />Github</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
-							variant="outline"><Icon icon="gg:vercel" class="h-4 w-4" />Vercel</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-primary hover:text-primary"
-							variant="outline"><Icon icon="gg:vercel" class="h-4 w-4" />z.com</Badge
-						>
-					</div>
-
-					<button
-						class="col-span-3 row-span-4 transition hover:scale-105 focus:outline-none"
-						onclick={() => toggleSection('designingTools')}
-					>
-						<div
-							class="flex cursor-pointer items-center justify-center gap-4 px-12 py-12 font-bold text-slate-400 hover:text-primary hover:underline md:px-24"
-						>
-							Tools I Use for Designing a Website
-							<MoveRight />
-						</div>
-					</button>
-				</div>
+			<div class=" grid grid-cols-2 gap-4 rounded-lg p-4 md:grid-cols-4">
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-purple-500 hover:text-purple-500"
+					variant="outline"><Icon icon="solar:figma-outline" class="h-4 w-4 " />Figma</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-500 hover:text-blue-500"
+					variant="outline"><Icon icon="teenyicons:framer-outline" class="h-4 w-4" />Framer</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-purple-500 hover:text-purple-500"
+					variant="outline"><Icon icon="hugeicons:adobe-xd" class="h-4 w-4" />Adobe XD</Badge
+				>
+				<Badge
+					class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-orange-500 hover:text-orange-500"
+					variant="outline"><Icon icon="solar:figma-outline" class="h-4 w-4 " />Figjam</Badge
+				>
 			</div>
-		{:else if section === 'designingTools'}
-			<div class="slide-in transform transition-transform">
-				<div class="text-xl font-bold text-primary md:text-3xl">
-					Here are the tools I use for designing websites:
-				</div>
-				<div>
-					<Separator class=" my-12 " />
-				</div>
-
-				<div class="grid grid-cols-1 gap-6 py-4 md:grid-cols-7">
-					<div class="col-span-4 row-span-4 grid grid-cols-2 gap-4 rounded-lg p-4 md:grid-cols-3">
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-purple-500 hover:text-purple-500"
-							variant="outline"><Icon icon="solar:figma-outline" class="h-4 w-4 " />Figma</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-blue-500 hover:text-blue-500"
-							variant="outline"
-							><Icon icon="teenyicons:framer-outline" class="h-4 w-4" />Framer</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-purple-500 hover:text-purple-500"
-							variant="outline"><Icon icon="hugeicons:adobe-xd" class="h-4 w-4" />Adobe XD</Badge
-						>
-						<Badge
-							class="flex items-center gap-2 px-4 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:border-orange-500 hover:text-orange-500"
-							variant="outline"><Icon icon="solar:figma-outline" class="h-4 w-4 " />Figjam</Badge
-						>
-					</div>
-
-					<button
-						class="col-span-3 row-span-4 transition hover:scale-105 focus:outline-none"
-						onclick={() => toggleSection('about')}
-					>
-						<div
-							class="flex cursor-pointer items-center justify-center gap-4 px-12 py-12 font-bold text-slate-400 hover:text-primary hover:underline md:px-24"
-						>
-							<MoveLeft />About Me
-						</div>
-					</button>
-				</div>
-			</div>
-		{/if}
+		</div>
 	</div>
 </main>
-
-<style>
-	.slide-in {
-		animation: slideIn 0.5s ease-in-out forwards;
-	}
-
-	@keyframes slideIn {
-		from {
-			transform: translateX(100%);
-			opacity: 0;
-		}
-		to {
-			transform: translateX(0);
-			opacity: 1;
-		}
-	}
-</style>
