462660d441a7e248ce279a6440eba6da01d0f16f
paul allan palacio <palaciopaulbermudez@gmail.com>
2025-02-09 12:04:09 +0800

    .

--- src/routes/(app)/design/framer/+page.svelte
+++ src/routes/(app)/design/framer/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import { SendHorizontalIcon, FileUserIcon, ExternalLink } from 'lucide-svelte';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 </script>
 
 <main class="space-24 container">
-	<!-- Web Development Projects -->
 	<div class="-mt-4 space-y-4">
 		<div>
 			<h1 class="text-slate-400">Web Design Projects</h1>
