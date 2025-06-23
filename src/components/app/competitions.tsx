7782ec6d229e7a90b633ce999e8c3a324d52d986
paul allan palacio <palaciopaulbermudez@gmail.com>
2025-06-23 21:15:04 +0800

    fix competitions

--- src/components/app/competitions.tsx
+++ src/components/app/competitions.tsx
@@ -1,4 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ShieldCheck } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Competitions() {
   return (
@@ -28,6 +31,17 @@ export default function Competitions() {
             provide solutions to a general problem, pitching it to actual
             industry representatives.
           </div>
+          <Link
+            href={
+              "https://www.linkedin.com/in/paulallanpalacio/details/certifications/"
+            }
+            target="_blank"
+          >
+            <Button className="mt-4">
+              <ShieldCheck />
+              View Certificate
+            </Button>
+          </Link>
         </li>
 
         <li className="relative ml-10 space-y-2 mb-12">
@@ -41,7 +55,9 @@ export default function Competitions() {
               <AvatarFallback>logo</AvatarFallback>
             </Avatar>
           </div>
-          <div className="text-sm text-muted-foreground">June 2023</div>
+          <div className="text-sm text-muted-foreground">
+            Dec 2023 -Jun 2024
+          </div>
           <div> AppCon 2023 | OTis Philippines Inc.</div>
 
           <div className="text-sm text-muted-foreground">
@@ -52,6 +68,17 @@ export default function Competitions() {
             provide participants with opportunities for recognition and societal
             contribution.
           </div>
+          <Link
+            href={
+              "https://www.linkedin.com/in/paulallanpalacio/details/certifications/"
+            }
+            target="_blank"
+          >
+            <Button className="mt-4">
+              <ShieldCheck />
+              View Certificate
+            </Button>
+          </Link>
         </li>
 
         <li className="relative ml-10 space-y-2 mb-12">
@@ -65,7 +92,7 @@ export default function Competitions() {
               <AvatarFallback>logo</AvatarFallback>
             </Avatar>
           </div>
-          <div className="text-sm text-muted-foreground">June 2023</div>
+          <div className="text-sm text-muted-foreground">Sep 2023</div>
           <div> Hack4Gov 2 CERT-PH Cyber Challenge | DICT</div>
 
           <div className="text-sm text-muted-foreground">
@@ -76,6 +103,17 @@ export default function Competitions() {
             provide participants with opportunities for recognition and societal
             contribution.
           </div>
+          <Link
+            href={
+              "https://www.linkedin.com/in/paulallanpalacio/details/certifications/"
+            }
+            target="_blank"
+          >
+            <Button className="mt-4">
+              <ShieldCheck />
+              View Certificate
+            </Button>
+          </Link>
         </li>
 
         <li className="relative ml-10 space-y-2 mb-12">
@@ -103,6 +141,51 @@ export default function Competitions() {
             provide solutions to a general problem, pitching it to actual
             industry representatives.
           </div>
+          <Link
+            href={
+              "https://www.linkedin.com/in/paulallanpalacio/details/certifications/"
+            }
+            target="_blank"
+          >
+            <Button className="mt-4">
+              <ShieldCheck />
+              View Certificate
+            </Button>
+          </Link>
+        </li>
+
+        <li className="relative ml-10 space-y-2 mb-12">
+          <div className="absolute -left-16 top-2 flex items-center justify-center bg-white rounded-full">
+            <Avatar className="border size-12 m-auto">
+              <AvatarImage
+                src="/img/founders-live.png"
+                alt="logo"
+                className="object-cover"
+              />
+              <AvatarFallback>logo</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="text-sm text-muted-foreground">Jul 2024</div>
+          <div> Founders Live Balanga</div>
+
+          <div className="text-sm text-muted-foreground">
+            Founders Live is a competition that brings together startup
+            founders, innovators, and the community for an exciting 90-second
+            pitch competition. In each event, selected entrepreneurs share their
+            big ideas in just 90 seconds — no slides, no fluff — followed by
+            live audience Q&A and voting.
+          </div>
+          <Link
+            href={
+              "https://www.linkedin.com/in/paulallanpalacio/details/certifications/"
+            }
+            target="_blank"
+          >
+            <Button className="mt-4">
+              <ShieldCheck />
+              View Certificate
+            </Button>
+          </Link>
         </li>
       </ul>
     </div>
